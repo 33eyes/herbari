f3bff24820113d7b7d5d32d3dc48902cf16847e8
Arina I <ariiigu@gmail.com>
2017-01-30 04:30:37 -0500

    Added Plant custom post type, edited posts query on front page, added wp masonry layout to front page.

---
+++ wp-content/themes/herbari/front-page.php
@@ -0,0 +1,99 @@
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap cf">
+
+						<main id="main" class="m-all t-2of3 d-all cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+							<?php if (have_posts()) : ?>
+							
+							<div id="masonry-container">
+								
+								<?php while (have_posts()) : the_post(); 
+								$featured_image = get_field("featured_image");
+								$size = "full"; 
+								?>
+								<div class="masonry-item">
+									<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+
+										<header class="article-header">
+
+											<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+											<p class="byline entry-meta vcard">
+																				<?php print(
+															/* the time the post was published */
+															'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>'
+														); ?>
+											</p>
+
+										</header>
+
+										<section class="entry-content cf">
+											<div class="plant-featured-image">
+												<?php if($featured_image) { 
+													echo wp_get_attachment_image( $featured_image, $size );
+												} ?>
+											</div>
+											<div class="plant-excerpt">
+												<?php the_excerpt(); ?>
+											</div>
+										</section>
+
+										<footer class="article-footer cf">
+											<p class="footer-comment-count">
+												<?php comments_number( '', __( '<span>1</span> Comment', 'bonestheme' ), __( '<span>%</span> Comments', 'bonestheme' ) );?>
+											</p>
+
+										</footer>
+
+									</article>
+									
+								</div>
+
+								<?php endwhile; ?>
+								
+							</div>
+							
+							<?php bones_page_navi(); ?>
+
+							<?php else : ?>
+
+									<article id="post-not-found" class="hentry cf">
+											<header class="article-header">
+												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+										</header>
+											<section class="entry-content">
+												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+										</section>
+										<footer class="article-footer">
+												<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
+										</footer>
+									</article>
+
+							<?php endif; ?>
+
+
+						</main>
+
+				</div>
+
+			</div>
+
+
+<?php get_footer(); ?>
+
+
+<!-- Wordpress Masonry script -->
+<script type="text/javascript">	
+
+	jQuery(window).load(function() {
+		var container = document.querySelector('#masonry-container');
+		var msnry = new Masonry( container, {
+			itemSelector: '.masonry-item',
+			columnWidth: '.masonry-item',                
+		});  
+  
+	});
+
+</script>
--- wp-content/themes/herbari/single-plants.php
+++ wp-content/themes/herbari/single-plants.php
@@ -16,14 +16,16 @@
 ?>
 
 <?php get_header(); ?>
-
 			<div id="content">
 
 				<div id="inner-content" class="wrap cf">
 
 						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+							<?php if (have_posts()) : while (have_posts()) : the_post(); 
+							$featured_image = get_field("featured_image");
+							$size = "full"; 
+							?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article">
 
@@ -37,29 +39,17 @@
 								</header>
 
 								<section class="entry-content cf">
-									<?php
-										// the content (pretty self explanatory huh)
-										the_content();
-
-										/*
-										 * Link Pages is used in case you have posts that are set to break into
-										 * multiple pages. You can remove this if you don't plan on doing that.
-										 *
-										 * Also, breaking content up into multiple pages is a horrible experience,
-										 * so don't do it. While there are SOME edge cases where this is useful, it's
-										 * mostly used for people to get more ad views. It's up to you but if you want
-										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-										 *
-										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-										 *
-										*/
-										wp_link_pages( array(
-											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-											'after'       => '</div>',
-											'link_before' => '<span>',
-											'link_after'  => '</span>',
-										) );
-									?>
+									<div class="plant-featured-image">
+										<?php if($featured_image) { 
+											echo wp_get_attachment_image( $featured_image, $size );
+										} ?>
+									</div>
+									<div class="plant-excerpt">
+										<?php the_excerpt(); ?>
+									</div>
+									<div class="plant-content">
+										<?php the_content(); ?>
+									</div>
 								</section> <!-- end article section -->
 
 								<footer class="article-footer">
@@ -83,7 +73,7 @@
 											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
 										</section>
 										<footer class="article-footer">
-											<p><?php _e( 'This is the error message in the single-custom_type.php template.', 'bonestheme' ); ?></p>
+											<p><?php _e( 'This is the error message in the single-plants.php template.', 'bonestheme' ); ?></p>
 										</footer>
 									</article>
 
