3af71ecc95a1024f26a9be5b93b5ebf5ddfbf159
Arina I <ariiigu@gmail.com>
2017-02-02 12:05:38 -0500

    Initial commit

---
+++ wp-content/themes/herbari/footer.php
@@ -0,0 +1,48 @@
+			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
+
+				<div id="inner-footer" class="wrap cf">
+
+					<nav role="navigation">
+						<?php wp_nav_menu(array(
+    					'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
+    					'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
+    					'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
+    					'menu_class' => 'nav footer-nav cf',            // adding custom nav class
+    					'theme_location' => 'footer-links',             // where it's located in the theme
+    					'before' => '',                                 // before the menu
+    					'after' => '',                                  // after the menu
+    					'link_before' => '',                            // before each link
+    					'link_after' => '',                             // after each link
+    					'depth' => 0,                                   // limit the depth of the nav
+    					'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
+						)); ?>
+					</nav>
+
+					<p class="source-org copyright text-center">
+						&copy; <?php echo date('Y'); ?> 
+						<?php bloginfo( 'name' ); ?>
+						| web development by <a href="http://www.arigu.me/">arigu.me</a>
+					</p>
+
+				</div>
+
+			</footer>
+
+		</div>
+		
+		<!-- SCROLL-TO-TOP BUTTON -->
+		<a href="#" class="go-top" style="display: none;"><i class="fa fa-angle-double-up fa-2x"></i></a>
+
+		
+		<?php // all js scripts are loaded in library/bones.php ?>
+		<?php wp_footer(); ?>
+
+		
+		<!-- Bootstrap CDN -->
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		
+	</body>
+
+</html> <!-- end of site. what a ride! -->
