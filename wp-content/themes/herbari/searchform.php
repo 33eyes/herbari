c816e33be16a8a24faa630c2ec25a0d5e1c3f8c1
Arina I <ariiigu@gmail.com>
2017-02-28 16:33:53 -0500

    Updated sidebar widgets styles.

--- wp-content/themes/herbari/searchform.php
+++ wp-content/themes/herbari/searchform.php
@@ -1,8 +1,9 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
-        <label for="s" class="screen-reader-text"><?php _e('Search for:','bonestheme'); ?></label>
-        <input type="search" id="s" name="s" value="" />
+        <!--<label for="s" class="screen-reader-text"><?php _e('Search for:','bonestheme'); ?></label>
+		-->
+		<input type="search" id="s" name="s" value="" placeholder="Search..." />
 
-        <button type="submit" id="searchsubmit" ><?php _e('Search','bonestheme'); ?></button>
+        <button type="submit" id="searchsubmit" ><i class="fa fa-search fa-2x" aria-hidden="true"></i></button>
     </div>
 </form>
