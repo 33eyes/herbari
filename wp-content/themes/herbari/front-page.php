836814ac74225113b352a72b0faed015a1a1b550
Arina I <ariiigu@gmail.com>
2017-02-28 17:19:44 -0500

    Adjusted responsive layouts for sidebar on front page.

--- wp-content/themes/herbari/front-page.php
+++ wp-content/themes/herbari/front-page.php
@@ -4,7 +4,7 @@
 
 				<div id="inner-content" class="wrap cf">
 
-						<main id="main" class="m-all t-all d-4of5 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+						<main id="main" class="m-all t-5of7 d-4of5 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
 							<?php if (have_posts()) : ?>
 							
